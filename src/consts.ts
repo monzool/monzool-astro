c1d11606fbf800a5a8d9b71d710e56763b6ab757
Jan Skriver Sørensen <jan.skriver.soerensen@gmail.com>
2026-09-22 11:19:22 +0200

    Personal details filled

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'monzool.net';
+export const SITE_DESCRIPTION = 'Monzool\'s Personal Publishing a/ Jan Skriver Sørensen';
